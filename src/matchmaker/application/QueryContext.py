010b17e72a494485eb1438c995d73ab055983847
sprint_ska <sprint_ska@hotmail.com>
2022-10-06 01:05:30 -0700

    More Flask config tshooting

--- src/matchmaker/application/QueryContext.py
+++ src/matchmaker/application/QueryContext.py
@@ -1,16 +1,17 @@
+import os
 import requests
 from flask import current_app as app
 
 
 def Query(operation_name, operation_doc, variables):
 
-    url = "https://{}".format(app.config["MATCHMAKER_HASURA_URL"])
+    url = "https://{}".format(os.environ.get("MATCHMAKER_HASURA_URL"))
     data = {
         "query": str(operation_doc),
         "variables": variables,
         "operationName": operation_name,
     }
-    headers = {"X-Hasura-Admin-Secret": app.config["HASURA_ADMIN_SECRET"]}
+    headers = {"X-Hasura-Admin-Secret": os.environ.get("HASURA_ADMIN_SECRET")}
 
     r = requests.post(url, json=data, headers=headers)
 
--- src/matchmaker/application/__init__.py
+++ src/matchmaker/application/__init__.py
@@ -7,7 +7,6 @@ def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_prefixed_env(prefix="MATCHMAKER_")
-    app.config["HASURA_URL"] = os.environ.get("MATCHMAKER_HASURA_URL")
 
     try:
         os.makedirs(app.instance_path)
